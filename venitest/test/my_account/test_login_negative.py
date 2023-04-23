0006fbf5ca062c2a73eb6c4406c8e685b60fec2e
Venislav Zdravkov <venislav.zdravkov@gmail.com>
2023-04-23 12:13:06 +0300

    adding some information message

--- venitest/test/my_account/test_login_negative.py
+++ venitest/test/my_account/test_login_negative.py
@@ -8,6 +8,10 @@ class TestLoginNegative:
     password = 'rfse'
     @pytest.mark.tcid12
     def test_login_none_existing_user(self):
+        print("************")
+        print("test ID 12")
+        print("This test checks logging with non-existing user")
+        print("***************************")
         # go to my account
         my_account = MyAccountSignedOut(self.driver)
         my_account.go_to_my_account()
